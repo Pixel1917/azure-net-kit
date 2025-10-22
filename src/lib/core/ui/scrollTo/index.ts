bb0b4a4312193d91136df34c9af648311df086be
Dante Must Die <sergey.galaktionov1917@yandex.ru>
2025-10-23 00:39:02 +0300

    feat: 🎸 scrollTo

---
+++ src/lib/core/ui/scrollTo/index.ts
@@ -0,0 +1,22 @@
+import { EnvironmentUtil } from 'azure-net-tools';
+
+export const scrollTo = (nodeOrName: string | Element, offset = 220) => {
+	if (EnvironmentUtil.isBrowser) {
+		let element: Element;
+		if (typeof nodeOrName === 'string') {
+			const node = document.querySelector(nodeOrName);
+			if (node) {
+				element = node;
+			}
+		} else {
+			element = nodeOrName;
+		}
+		if (element!) {
+			const obj_pos = element.getBoundingClientRect();
+			window.scrollTo({
+				top: obj_pos.top + scrollY - offset,
+				behavior: 'smooth'
+			});
+		}
+	}
+};
